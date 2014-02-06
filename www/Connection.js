968d7fe9d9838f2a3607a9409bf6ea8a1a7e5f66
sgrebnov <sergei.grebnov@gmail.com>
2014-02-07 02:46:38 +0400

    comunication via 3rd party bluetoothSerial plugin

--- www/Connection.js
+++ www/Connection.js
@@ -21,59 +21,28 @@
 
 var Promise = require('./Promise');
 
-var Connection = function() {
-    
-    this.ev3Channel = null;
-    this.ev3Service = null;
-    this.dataWriter = null;
-}
+var Connection = function () { };
 
-Connection.prototype.connectAsync = function(brickName) {
+Connection.prototype.connectAsync = function (brickName) {
     var deferral = new Promise.Deferral();
-    var onSuccess = function(res) {
+    var onSuccess = function (res) {
         deferral.resolve(res);
     };
-    var onError = function(err) {
+    var onError = function (err) {
         deferral.resolve(err);
-    }
+    };
 
-    var me = this;
+    setTimeout(function () {
+        try {
+            bluetoothSerial.connect(brickName, onSuccess, onError);
+        } catch (ex) {
+            onError(ex);
+        }
 
-    // execute async
-    setTimeout(function() {
-            var rfcomm = Windows.Devices.Bluetooth.Rfcomm;
-            var sockets = Windows.Networking.Sockets;
-            var streams = Windows.Storage.Streams;
-            var query = rfcomm.RfcommDeviceService.getDeviceSelector(rfcomm.RfcommServiceId.serialPort);
-
-            Windows.Devices.Enumeration.DeviceInformation.findAllAsync(query)
-                .then(function (pairedDevices) {
-
-                    return new WinJS.Promise(function (complete, error, progress) {
-                        for (var idx = 0; idx < pairedDevices.length; idx++) {
-                            if (pairedDevices[idx].name.toLowerCase() == brickName) {
-                                rfcomm.RfcommDeviceService.fromIdAsync(pairedDevices[idx].id).then(function (device) {
-                                    if (device != null) {
-                                        complete(device);
-                                    }
-                                });
-                            }
-                        };
-                    });
-
-                }).then(function(ev3) {
-                    me.ev3Service = ev3;
-                    me.ev3Channel = new sockets.StreamSocket();
-                    return me.ev3Channel.connectAsync(me.ev3Service.connectionHostName, me.ev3Service.connectionServiceName, sockets.SocketProtectionLevel.plainSocket);
-            }).done(
-                function() { // success
-                    me.dataWriter = new streams.DataWriter(me.ev3Channel.outputStream);
-                    onSuccess();
-                }, onError);
-    },0);
+    }, 0);
 
     return deferral.promise;
-}
+};
 
 Connection.prototype.sendAsync = function(command) {
     var deferral = new Promise.Deferral(),
@@ -86,23 +55,14 @@ Connection.prototype.sendAsync = function(command) {
     var onSuccess = function(res) {
         deferral.resolve(res);
     };
-
-    if (this.dataWriter == null) {
-        onError(new Error('Not connected'));
-        return deferral.promise;
-    }
-
-    var dataWriter = this.dataWriter;
     // execute async
     setTimeout( function() {
         try {
             var header = [];
                 header[0] = bytes.length % 256;
                 header[1] = 0;// TODO: payload.length / 256
-                dataWriter.writeBytes(header);
-                dataWriter.writeBytes(bytes);
-                dataWriter.storeAsync().done(onSuccess, onError);
 
+            bluetoothSerial.write(header.concat(bytes), onSuccess, onError);
         } catch (ex) {
             onError (ex);
         }
